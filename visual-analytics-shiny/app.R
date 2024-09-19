6750b4f62a90106d48acf3ca5a9acb2cd36a18a6
Sven Ligensa <sven.ligensa@gmail.com>
2024-09-20 00:50:32 +0200

    Added navigation bar

--- visual-analytics-shiny/app.R
+++ visual-analytics-shiny/app.R
@@ -3,65 +3,80 @@ library(shinyjs)
 library(shinyWidgets)
 library(bslib)
 
-ui <- page_sidebar(
-  useShinyjs(),
+ui <- page_navbar(
 
-  # Links to external CSS and JS code
-  tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css"),
-    tags$script(src = "script.js")
-  ),
-  
-  tags$div(
+  title = tags$div(
     class = "got-font",
-    tags$h1("Game of Thrones Analyzer")
+    "Game of Thrones Analyzer"
   ),
 
-  sidebar = div(
-    card(
-      card_header(h4("Show locations")),
-      selectizeInput(
-        inputId = "location",
-        label = "Select locations:",
-        choices = c(),
-        multiple = TRUE,
-        options = list(
-          placeholder = 'Type to search...',
-          maxOptions = 5
+  header = list(
+    useShinyjs(),
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "styles.css"),
+      tags$script(src = "script.js")
+    )
+  ),
+
+  nav_panel(
+    title = "Map",
+    layout_sidebar(
+      sidebar = div(
+        card(
+          card_header(h4("Show locations")),
+          selectizeInput(
+            inputId = "location",
+            label = "Select locations:",
+            choices = NULL,  # Choices will be updated server-side
+            multiple = TRUE,
+            options = list(
+              placeholder = 'Type to search...',
+              maxOptions = 5
+            )
+          ),
+          actionButton("show", label = "Show"),
+          actionButton("hide", label = "Hide")
         )
       ),
-      actionButton("show", label = tagList("Show")),
-      actionButton("hide", label = tagList("Hide"))
-    ),
-  ),
-  
-  div(
-    class = "content-container",
-    card(
-      class = "map-card",
-      actionButton(
-        inputId = "toggle_fit",
-        label = tags$img(src = "fit_h.png", height = "40px"), # https://www.flaticon.com/free-icon/fit_2012194
-        class = "fit-toggle-button"
-      ),
-      div(id = "map-container",
-          div(class = "map-wrapper",
+      # Main content area
+      div(
+        class = "content-container",
+        card(
+          class = "map-card",
+          actionButton(
+            inputId = "toggle_fit",
+            label = tags$img(src = "fit_h.png", height = "40px"),
+            class = "fit-toggle-button"
+          ),
+          div(id = "map-container",
+            div(class = "map-wrapper",
               tags$img(id = "map-img", src = "map.png"),
-              tags$svg(id = "map-canvas"),
+              tags$svg(id = "map-canvas")
+            )
           )
+        )
       )
+    )
+  ),
+
+  nav_panel(
+    title = "Network",
+    card(
+      h2("Coming soon...")
     )
   )
 )
 
 server <- function(input, output, session) {
-
+  
+  # Load city data
   city_data <- read.csv("city_data.csv", stringsAsFactors = FALSE)
-
+  
+  # Update the selectizeInput choices
   locations <- city_data[["location"]]
   updateSelectizeInput(session, "location", choices = locations, server = TRUE)
-
-  # Initialize fit_mode reactiveVal
+  
+  # Initialize fit_mode reactive value
   fit_mode <- reactiveVal("Width")
   
   # Observe the toggle_fit button
@@ -85,20 +100,23 @@ server <- function(input, output, session) {
       addClass("map-container", "fit-width")
       removeClass("map-container", "fit-height")
     }
-    # Trigger the updateSVGSize function to adjust the SVG
+    # Adjust the SVG overlay
     runjs("updateSVGSize();")
   })
-
+  
+  # Show selected cities on the map
   observeEvent(input$show, {
     selected_cities <- city_data[city_data$location %in% input$location, ]
-
     for (i in 1:nrow(selected_cities)) {
       city <- selected_cities[i, ]
-      runjs(sprintf("showCity(%f, %f, %f, '%s', %f, '%s');", 
-                    city$point_x, city$point_y, city$radius, city$color, city$font_size, city$location))
+      runjs(sprintf(
+        "showCity(%f, %f, %f, '%s', %f, '%s');",
+        city$point_x, city$point_y, city$radius, city$color, city$font_size, city$location
+      ))
     }
   })
   
+  # Hide annotations on the map
   observeEvent(input$hide, {
     runjs("hideAnnotations();")
   })
